072d5402b33dbb7f1458b8be0eae4b6c7936c935
RomanGlegola <romanglegola@gmail.com>
2024-10-07 22:55:04 +0200

    DATA: introduce audio_noise
    Introduce audio_noise static constants.
    
    Signed-off-by: RomanGlegola <romanglegola@gmail.com>

---
+++ repozytoria/generate-wavs/data/audio_noise.py
@@ -0,0 +1,13 @@
+noise_type: dict[str, tuple[str, ...]] = {
+    "none": ("", ),
+    "white": ("white", ),
+    "pink": ("pink", ),
+}
+
+noise_volume: dict[str, tuple[float, ...]] = {
+    "none": (0.0, ),
+    "low": (0.1, ),
+    "mid": (0.3, ),
+    "high": (0.5, ),
+    "full": (1.0, ),
+}
