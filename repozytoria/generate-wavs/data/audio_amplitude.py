f197ef58d41f3f9415a355c34d519f8ab94c1b88
RomanGlegola <romanglegola@gmail.com>
2024-10-07 22:11:05 +0200

    DATA: audio data
    Added audio data to generate wav files.
    
    Signed-off-by: RomanGlegola <romanglegola@gmail.com>

---
+++ repozytoria/generate-wavs/data/audio_amplitude.py
@@ -0,0 +1,5 @@
+amplitude: dict[str, tuple[float, ...]] = {
+    "low": (0.5, ),
+    "standard": (0.5, ),
+    "high": (1.0, ),
+}
---
+++ repozytoria/generate-wavs/data/audio_phase_shift.py
@@ -0,0 +1,5 @@
+phase_shift: dict[str, tuple[float, ...]] = {
+    "none": (0.0, ),
+    "quarter": (45.0, ),
+    "half": (90.0, ),
+}
