35b6316d0dd98f43c53bb051cd966c455db284a0
RomanGlegola <romanglegola@gmail.com>
2024-10-07 23:29:35 +0200

    REFACTOR: change module name
    Change module name
    Implement PyLint
    Add main.py
    
    Signed-off-by: RomanGlegola <romanglegola@gmail.com>

---
+++ repozytoria/generate_wavs/main.py
@@ -0,0 +1,41 @@
+import argparse
+import subprocess
+from itertools import product
+
+from data.sound_scenario import scenarios
+
+
+def run_rust_program(executable_path, **kwargs):
+    args = [executable_path]
+
+    for key, value in kwargs.items():
+        args.append(f"--{key}")
+        args.append(str(value))
+    subprocess.run(args, check=True)
+
+
+def execute_scenario(executable_path, settings):
+    keys, values = zip(*settings.items())
+    for combination in product(*values):
+        combination_dict = dict(zip(keys, combination))
+        run_rust_program(executable_path, **combination_dict)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Run the Rust program with different scenarios"
+    )
+    parser.add_argument(
+        "scenario", choices=scenarios.keys(), help="Choose a scenario"
+    )
+    parser.add_argument(
+        "executable_path", help="Path to the compiled Rust executable"
+    )
+    args = parser.parse_args()
+
+    scenario_settings = scenarios[args.scenario]
+    execute_scenario(args.executable_path, scenario_settings)
+
+
+if __name__ == "__main__":
+    main()
